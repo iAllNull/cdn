3479b4f772d6c36b8dc1b18da4961a93be4a800a
Null <89527440+iAllNull@users.noreply.github.com>
2021-09-17 16:38:03 +0800

    Add files via upload

---
+++ keddy/layuiadmin/api.js
@@ -0,0 +1,69 @@
+function ICP() {//ICP备案API
+    var cou= layer.open({
+    title :'调用信息',
+    type: 2,
+    content: '/API/ICP/index.php',
+    area: ['100%', '100%'],
+    });
+    layer.full(cou);
+}function Dwz() {//短网址接口
+    var cou= layer.open({
+    title :'调用信息',
+    type: 2,
+    content: '/API/Dwz/index.php',
+    area: ['100%', '100%'],
+    });
+    layer.full(cou);
+
+}function Title() {//标题获取
+    var cou= layer.open({
+    title :'调用信息',
+    type: 2,
+    content: '/API/Title/index.php',
+    area: ['100%', '100%'],
+    });
+    layer.full(cou);
+}function Bing() {//随机背景图API
+    var cou= layer.open({
+    title :'调用信息',
+    type: 2,
+    content: '/API/Bing/index.php',
+    area: ['100%', '100%'],
+    });
+    layer.full(cou);
+}function State() {//电脑在线接口
+    var cou= layer.open({
+    title :'调用信息',
+    type: 2,
+    content: '/API/State/index.php',
+    area: ['100%', '100%'],
+    });
+    layer.full(cou);
+}function Music() {//音乐解析接口
+    var cou= layer.open({
+    title :'调用信息',
+    type: 2,
+    content: '/API/Music/index.php',
+    area: ['100%', '100%'],
+    });
+    layer.full(cou);
+}function Qun() {//头像链接接口
+    var cou= layer.open({
+    title :'调用信息',
+    type: 2,
+    content: '/API/Qun/index.php',
+    area: ['100%', '100%'],
+    });
+    layer.full(cou);
+}function Qrcode() {//Qrcode二维码接口
+    var cou= layer.open({
+    title :'调用信息',
+    type: 2,
+    content: '/API/Qrcode/index.php',
+    area: ['100%', '100%'],
+    });
+    layer.full(cou);
+}function update(){//后续……
+	layer.msg("API接口还在火热更新中！",{icon:3},{title:"敬请期待",});
+}
+
